7d546704f54ab264bd3bbdb2e48a9880866fa800
vladlen <reactivehermes@gmail.com>
2019-11-03 19:13:58 +0300

    test finished

--- index.js
+++ index.js
@@ -14,7 +14,15 @@ let buttonVisibilityTimeOut;
 window.onload = () => {
     BUTTON_MINUS_ROW.style.display = "none";
     BUTTON_MINUS_COLUMN.style.display = "none";
-}
+    
+    for(let i = 0; i <= 3; i++) {
+       let elementRow = TABLE.insertRow();
+       
+       for(let j = 0; j <= 3; j++) {
+           elementRow.insertCell();
+       };
+    };
+};
 
 BUTTON_PLUS_ROW.onclick = function () {
     let currentRow = TABLE.insertRow();
@@ -28,6 +36,7 @@ BUTTON_PLUS_ROW.onclick = function () {
 
 BUTTON_PLUS_COLUMN.onclick = function () {
     let iterationsRow = TABLE.rows.length;
+    
     for (let i = 0; i < iterationsRow; i++) {
         TABLE.rows[i].insertCell();
     };
@@ -39,11 +48,14 @@ TABLE.onmouseover = function (event) {
     let targetRow = target.offsetTop;
     let targetColumn = target.offsetLeft;
     clearTimeout(buttonVisibilityTimeOut);
-    console.log('over');
 
-    BUTTON_MINUS_ROW.style.display = "block";
+    if (TABLE.rows.length > 1) {
+        BUTTON_MINUS_ROW.style.display = "block";
+    };
+    if (TABLE.rows[0].cells.length > 1) {
     BUTTON_MINUS_COLUMN.style.display = "block";
-    console.log(event.target);
+    };
+
     if (target.tagName != 'TD') return;
     currentRow = target.parentElement.rowIndex;
     currentCell = target.cellIndex;
@@ -58,28 +70,41 @@ function hideMinusButtons() {
 }
 
 TABLE.onmouseout = () => {
-    console.log('out');
-    buttonVisibilityTimeOut = setTimeout(hideMinusButtons, 2000);
+    buttonVisibilityTimeOut = setTimeout(hideMinusButtons, 1000);
 }
 
 BUTTON_MINUS_ROW.onclick = () => {
-    let r = TABLE.rows.length;
-    
-    if (r == 1) return;
+    let iterationsRow = TABLE.rows.length;
+    let hiden = setTimeout(hideMinusButtons, 0);
+    if (iterationsRow == 1) return;
     TABLE.deleteRow(currentRow);
+    BUTTON_MINUS_ROW.style.top = TABLE.rows[0].cells[0].offsetTop + "px";
+    
 }
 
 BUTTON_MINUS_COLUMN.onclick = ()=> {
-    let iterations = TABLE.rows.length;
-    let c = TABLE.rows[0].cells.length;
-    if (c == 1) return;
-    for(i = 0; i < iterations; i++) {
+    let iterationsRow = TABLE.rows.length;
+    let iterationsCell = TABLE.rows[0].cells.length;
+    let hiden = setTimeout(hideMinusButtons, 0);
+    if (iterationsCell == 1) return;
+    for(i = 0; i < iterationsRow; i++) {
         TABLE.rows[i].deleteCell(currentCell);
-    }
-}
+    };
+    BUTTON_MINUS_COLUMN.style.left = TABLE.rows[0].cells[0].offsetLeft + "px";
+};
+
 BUTTON_MINUS_ROW.onmouseover = function () {
     clearTimeout(buttonVisibilityTimeOut);
-}
+};
+
 BUTTON_MINUS_COLUMN.onmouseover = function () {
     clearTimeout(buttonVisibilityTimeOut);
-}
+};
+
+BUTTON_MINUS_ROW.onmouseout = function () {
+    buttonVisibilityTimeOut = setTimeout(hideMinusButtons, 1000);
+};
+
+BUTTON_MINUS_COLUMN.onmouseout = function () {
+    buttonVisibilityTimeOut = setTimeout(hideMinusButtons, 1000);
+};
